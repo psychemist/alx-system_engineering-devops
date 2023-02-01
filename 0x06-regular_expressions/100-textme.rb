432d3f0cba3fa3190d0714a2986a724a49e5a9a6
psych0wh1z <ikeagudike@gmail.com>
2023-02-01 05:58:23 +0100

    Remove regex options

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+?\w+)\]\s*\[to:(\+?\d+)\]\s*\[flags:((\-\d+(:\d:)*)+)/gm).join(',')
+puts ARGV[0].scan(/\[from:(\+?\w+)\]\s*\[to:(\+?\d+)\]\s*\[flags:((\-\d+(:\d:)*)+)/).join(',')
