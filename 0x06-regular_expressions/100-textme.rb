4323a1968cf11955708b19087b74030d2bc21073
psych0wh1z <ikeagudike@gmail.com>
2023-02-01 06:10:37 +0100

    Fix lazy and group matching

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+?\w+)\]\s*\[to:(\+?\d+)\]\s*\[flags:(\-\d:\d:\-\d:\d:\-\d)/).join(',')
+puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(',')
