5c1dde7e36774922e8bfe49cf5efac367dd04a26
psych0wh1z <ikeagudike@gmail.com>
2023-04-06 21:47:13 +0100

    Ensure commands run in order

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,16 +4,16 @@ exec { 'get update':
   command => '/usr/bin/apt-get update',
 }
 
-package { 'nginx':
-  ensure => 'present',
+-> package { 'nginx':
+   ensure => 'present',
 }
 
-file_line { 'add_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+-> file_line { 'add_header':
+   path  => '/etc/nginx/nginx.conf',
+   match => 'http {',
+   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
 
-exec { 'start':
-  command => 'usr/sbin/service nginx restart',
+-> exec { 'start':
+   command => 'usr/sbin/service nginx restart',
 }
