d98e33ce5ae9f5961394ac29272c4fcd2610f72d
psych0wh1z <ikeagudike@gmail.com>
2023-04-06 21:50:00 +0100

    Shorten script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,9 @@
 # automates the creation of a custom HTTP header response on web servers
 
-exec { 'get update':
-  command => '/usr/bin/apt-get update',
-}
-
--> package { 'nginx':
-   ensure => 'present',
-}
-
--> file_line { 'add_header':
-   path  => '/etc/nginx/nginx.conf',
-   match => 'http {',
-   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-}
-
--> exec { 'start':
-   command => 'usr/sbin/service nginx restart',
+exec { 'installation':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
