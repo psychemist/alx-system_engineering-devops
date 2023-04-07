43c03bada377300ff767b751bf4c8b7501c23215
psych0wh1z <ikeagudike@gmail.com>
2023-04-07 15:29:31 +0100

    Add root permissions

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # automates the creation of a custom HTTP header response on web servers
 
 exec { 'installation':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
+  command  => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
   sudo sed -i "/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
+  sudo service nginx restart',
   provider => shell,
 }
