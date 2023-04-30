044bf1d00f6e0b49a3ea3bbfbefc75568437377a
psych0wh1z <ikeagudike@gmail.com>
2023-04-30 08:05:05 +0100

    Write satisfactory script

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,8 +6,23 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos/".
-    users = requests.get("https://jsonplaceholder.typicode.com/users/}".
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(argv[1]))
+    todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/"
+                         .format(argv[1]))
 
-    for todo in r.json():
-        # print("".format(todo.get('id'), todo.userId, todo.title, todo.completed))
+    name = (user.json().get('name'))
+    task_done = task_todo = 0
+
+    for todo in todos.json():
+        if todo.get('completed') is True:
+            task_done += 1
+        else:
+            task_todo += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, task_done, task_todo + task_done))
+
+    for todo in todos.json():
+        if todo.get('completed') is True:
+            print("\t {}".format(todo.get('title')))
