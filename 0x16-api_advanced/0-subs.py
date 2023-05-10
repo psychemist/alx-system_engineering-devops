6a781a2ad271a009b956596aea356dc206c411b8
psych0wh1z <ikeagudike@gmail.com>
2023-05-10 05:59:30 +0100

    fix docs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,15 @@
+"""Queries redit api and returns blanc sjdkvbd jk
+"""
 import requests
 
 def number_of_subscribers(subreddit):
+    """Queries API and returns number of subscribers
+    Args:
+        subreddit (str): subreddit to be queried
+
+    Returns:
+        int: number of subs
+        """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-agent': 'Mozilla/5.0'}  # Add a user-agent header to prevent 429 error
 
