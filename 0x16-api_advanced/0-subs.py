b3b82bcda51a6f19999387d518aa7fbed7702d2d
psych0wh1z <ikeagudike@gmail.com>
2023-05-12 06:55:41 +0100

    Fix return value for None type

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,10 +17,9 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
 
-    res = requests.get(url, headers=headers)
-    data = res.json()
-    count = data.get('data').get('subscribers')
+    data = requests.get(url, headers=headers).json()
+    count = data.get('data', {}).get('subscribers')
 
-    if count is None:
+    if not count:
         return 0
     return count
