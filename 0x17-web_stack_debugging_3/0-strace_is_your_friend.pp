782d8cd427d7996251ccd5c7a28e301e990452aa
psych0wh1z <ikeagudike@gmail.com>
2023-05-23 19:04:58 +0100

    Delete subrepos for deferment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# Fix typo in config files of Apache server
-exec { 'fix config':
-  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
